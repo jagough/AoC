14f222ec035c7ccf8d60d686429ae95ce8ca2d52
Jonathan Gough <eridanus.gough@gmail.com>
2021-12-13 12:42:04 +0000

    Adds day 13 challenge solution solution

---
+++ 2021/Day13/Program.cs
@@ -0,0 +1,81 @@
+﻿var dots = File
+    .ReadAllLines("PuzzleInput.txt")
+    .Dots();
+var folds = File
+    .ReadAllLines("PuzzleInput.txt")
+    .Folds();
+
+var part1 = dots.Fold(folds.First()).Distinct();
+
+Console.WriteLine($"{part1.Count()}");
+
+var part2 = dots.Fold(folds).Distinct().Print();
+
+Console.WriteLine("Part 2:");
+Console.WriteLine(part2); // For some reason this is mirrored
+static class Extensions
+{
+    public static string Print(this IEnumerable<(int x, int y)> dots)
+    {
+        var sb = new System.Text.StringBuilder();
+
+        for (int y = 0; y <= dots.Max(d => d.y); y++)
+        {
+            for (int x = 0; x <= dots.Max(d => d.x); x++)
+            {
+                sb.Append(dots.Any(d => d.x==x && d.y==y) ? '#' : '.');
+            }
+            sb.AppendLine();
+        }
+        return sb.ToString();
+    }
+    public static IEnumerable<(int x, int y)> Dots(this IEnumerable<string> lines)
+    {
+        return lines
+            .Where(line => !line.StartsWith("fold") && line != string.Empty)
+            .Select(line => line
+                .Split(new[] { ',' })
+                .Select(x => Convert.ToInt32(x))
+                .ToArray())
+            .Select(d => (d[0], d[1]))
+            .ToArray();
+    }
+
+    public static IEnumerable<(bool axis, int fold)> Folds(this IEnumerable<string> lines)
+    {
+        return lines
+            .Where(line => line.StartsWith("fold"))
+            .Select(line => line.Split(' ').Last().Split('='))
+            .Select(x => (x[0] == "x", Convert.ToInt32(x[1])));
+    }
+
+    public static IEnumerable<(int x, int y)> Fold(this IEnumerable<(int x, int y)> dots, IEnumerable<(bool axis, int fold)> folds)
+    {
+        foreach (var fold in folds)
+        {
+            dots = dots
+                .Fold(fold)
+                .Distinct()
+                .ToArray();
+        }
+        return dots;
+    }
+    public static IEnumerable<(int x, int y)> Fold(this IEnumerable<(int x, int y)> dots, (bool axis, int fold) fold)
+    {
+        return dots.SelectMany(d => fold.axis ? d.FoldX(fold.fold) : d.FoldY(fold.fold));
+    }
+
+    public static IEnumerable<(int x, int y)> FoldY(this (int x, int y) dot, int fold)
+    {
+        if (fold == dot.y ) yield break;
+        if (fold > dot.y ) yield return dot;
+        if (fold < dot.y) yield return (dot.x, 2 * fold - dot.y);
+    }
+
+    public static IEnumerable<(int x, int y)> FoldX(this (int x, int y) dot, int fold)
+    {
+        if (fold == dot.x) yield break;
+        if (fold < dot.x) yield return (dot.x - (fold + 1), dot.y);
+        if (fold > dot.x) yield return (2 * fold - dot.x - (fold + 1), dot.y);
+    }
+}
---
+++ 2021/Day13/Tests.cs
@@ -0,0 +1,86 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Day13;
+
+
+public class Tests
+{
+    [Fact]
+    public void Assert_Dots_ReadOk()
+    {
+        var dots = File
+           .ReadAllLines("TestInput.txt")
+           .Dots()
+           .Print();
+
+@"...#..#..#.
+....#......
+...........
+#..........
+...#....#.#
+...........
+...........
+...........
+...........
+...........
+.#....#.##.
+....#......
+......#...#
+#..........
+#.#........
+".Should().BeEquivalentTo(dots);
+    }
+
+    [Fact]
+    public void Assert_Dots_FoldsOk()
+    {
+        var dots = File
+           .ReadAllLines("TestInput.txt")
+           .Dots();
+
+        var folds = File
+           .ReadAllLines("TestInput.txt")
+           .Folds();
+
+        var result = dots
+            .Fold(folds)
+            .Print();
+
+        @"#####
+#...#
+#...#
+#...#
+#####
+".Should().BeEquivalentTo(result);
+    }
+
+    [Theory]
+    [InlineData(0, 3, 0)]
+    [InlineData(1, 3, 1)]
+    [InlineData(2, 3, 2)]
+    [InlineData(6, 3, 0)]
+    [InlineData(5, 3, 1)]
+    [InlineData(4, 3, 2)]
+    [InlineData(6, 5, 4)]
+    [InlineData(4, 5, 4)]
+    public void Assert_FoldY_Ok(int y, int fold, int result)
+    {
+        (0, y).FoldY(fold).Should().Contain((0, result));
+    }
+
+    [Theory]
+    [InlineData(6, 3, 2)]
+    [InlineData(5, 3, 1)]
+    [InlineData(4, 3, 0)]
+    [InlineData(2, 3, 0)]
+    [InlineData(1, 3, 1)]
+    [InlineData(0, 3, 2)]
+    [InlineData(0, 2, 1)]
+    [InlineData(1, 2, 0)]
+    [InlineData(3, 2, 0)]
+    public void Assert_FoldXEven_Ok(int x, int fold, int result)
+    {
+        (x, 0).FoldX(fold).Should().Contain((result, 0));
+    }
+}
